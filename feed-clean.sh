ff6ccf990bd85d8da7a09793266cf83d04cf11ce
mapdio <77219481+mapdio@users.noreply.github.com>
2021-03-09 11:19:27 +0800

    Update feed-clean.sh

--- feed-clean.sh
+++ feed-clean.sh
@@ -7,4 +7,4 @@
 rm -rf feeds/packages/net/kcptun
 rm -rf feeds/kenzok8/redsocks2
 
-
+sed -i '/endef/a\TARGET_LDFLAGS+=-L$(STAGING_DIR)\/usr\/lib -L$(STAGING_DIR)\/lib' ./feeds/packages/lang/python/python3/files/python3-package-ctypes.mk
