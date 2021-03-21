ff468314c731257d15828ac7e6ab3465296af1c2
mapdio <77219481+mapdio@users.noreply.github.com>
2021-03-21 23:51:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,12 +12,19 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
 mv package/luci-app-cpufreq/po/zh-cn  package/luci-app-cpufreq/po/zh-Hans
 mv package/luci-app-unblockmusic/po/zh-cn package/luci-app-unblockmusic/po/zh-Hans
 mv package/luci-app-flowoffload/po/zh-cn package/luci-app-flowoffload/po/zh-Hans
 mv package/luci-app-autoreboot/po/zh-cn package/luci-app-autoreboot/po/zh-Hans
 mv package/luci-app-arpbind/po/zh-cn package/luci-app-arpbind/po/zh-Hans
 mv package/luci-app-accesscontrol/po/zh-cn package/luci-app-accesscontrol/po/zh-Hans
+
+
+cat >> luci/modules/luci-base/po/zh-cn/base.po <<EOF
+msgid "NAS"
+msgstr "网络存储"
+EOF
 
 
 
