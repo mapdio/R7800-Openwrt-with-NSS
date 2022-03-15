35bb7df8dce639362ef27b3ae2ef7be5ce65c16d
mapdio <77219481+mapdio@users.noreply.github.com>
2022-03-15 18:18:48 +0800

    Update feed-clean.sh

--- feed-clean.sh
+++ feed-clean.sh
@@ -7,12 +7,14 @@ cp -rf feeds/leanpack/multimedia/UnblockNeteaseMusic feeds/packages/multimedia/U
 cp -rf feeds/leanpack/net/baidupcs-web feeds/packages/net/baidupcs-web
 cp -rf feeds/leanpack/net/dnsforwarder feeds/packages/net/dnsforwarder
 cp -rf feeds/leanpack/net/dnsproxy feeds/packages/net/dnsproxy
+cp -rf feeds/leanpack/net/pdnsd-alt feeds/packages/net/pdnsd-alt
 
 rm -rf feeds/leanpack
 ./scripts/feeds update -a
 
 # 移除feed里重复组件
 
+rm -rf feeds/small/pdnsd-alt
 # rm -rf feeds/packages/net/kcptun
 # rm -rf feeds/kenzok8/redsocks2
 
